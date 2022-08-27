e75ee9a2e15719d3a41b4f5ec63add5216b7266d
Sukhvinder Singh <sukhvisukh@gmail.com>
2022-08-27 21:38:49 +0530

    configured babel and babel-loader

--- webpack.common.js
+++ webpack.common.js
@@ -1,14 +1,23 @@
 const PATH = require("path");
 
 module.exports = {
-
     entry: {
         "main": PATH.resolve(__dirname, "src/js/main.js"),
     },
-
     output: {
         path: PATH.resolve(__dirname, "dist/js"),
         filename: "[name].js"
+    },
+    target: ["web", "es5"],
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            }
+        ]
     }
-
 }
