cb15a04164b3590dd5c683a92ff8cd97e030fa2c
Sukhvinder Singh <sukhvisukh@gmail.com>
2022-08-27 23:23:28 +0530

    html webpack plugin and ejs loader configured and tested

--- webpack.common.js
+++ webpack.common.js
@@ -1,9 +1,18 @@
 const PATH = require("path");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const RemoveEmptyScriptsPlugin = require('webpack-remove-empty-scripts');
+const HTMLWebpackPlugin = require("html-webpack-plugin");
 
 const JS_OUTPUT_DIR = "./dist/js";
-const CSS_OUTPUT_DIR = PATH.relative(JS_OUTPUT_DIR, "./dist/css")
+const CSS_OUTPUT_DIR = PATH.relative(JS_OUTPUT_DIR, "./dist/css");
+
+const HTMLWebpackPluginInstance = new HTMLWebpackPlugin({
+    filename: "../index.html",
+    template: PATH.resolve(__dirname, "./src/html/template.ejs"),
+    publicPath: "js",
+    scriptLoading: "defer",
+    title: "Vue resume builder",
+});
 
 module.exports = {
     entry: {
@@ -18,6 +27,28 @@ module.exports = {
     module: {
         rules: [
             {
+                test: /\.ejs$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: "html-loader",
+                        options: {
+                            sources: false,
+                            esModule: false,
+                            minimize: false
+                        }
+                    },
+                    {
+                        loader: "template-ejs-loader",
+                        options: {
+                            data: {
+                                htmlWebpackPlugin: HTMLWebpackPluginInstance
+                            }
+                        }
+                    }
+                ],
+            },
+            {
                 test: /\.js$/,
                 exclude: /node_modules/,
                 use: {
@@ -26,6 +57,7 @@ module.exports = {
             },
             {
                 test: /\.s[ac]ss$/,
+                exclude: /node_modules/,
                 use: [MiniCssExtractPlugin.loader, 'css-loader', {
                     loader: "sass-loader",
                     options: {
@@ -36,6 +68,7 @@ module.exports = {
         ]
     },
     plugins: [
+        HTMLWebpackPluginInstance,
         new RemoveEmptyScriptsPlugin(),
         new MiniCssExtractPlugin({
             filename: ({ chunk }) => `${CSS_OUTPUT_DIR}/${chunk.name.replace('/js/', '/css/')}.css`,
