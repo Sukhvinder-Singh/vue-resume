8259f84c0e2c93d9363dcaf7aecbc57860f02f58
Sukhvinder Singh <sukhvisukh@gmail.com>
2022-08-29 01:45:57 +0530

    Added dummy data in projects

--- src/App/components/Projects.vue
+++ src/App/components/Projects.vue
@@ -1,8 +1,81 @@
 <template>
   <article class="project resume__article">
     <div class="resume__articleHeading">
-      <h3>Project name</h3>
-      <span>[<a href="">Live link</a>]</span>
+      <h3>Emerging giants in Asia-Pacific</h3>
+      <span>[<a href="" target="_blank">Live link</a>]</span>
     </div>
+    <ul>
+      <li>UI components to show top 100 startups in Asia-Pacific region.</li>
+      <li>
+        Developed custom interactive UI components &ndash;
+        <strong>Flip cards</strong>, <strong>Accordions</strong>,
+        <strong>Pagination, Modals</strong>, and <strong>Tabs</strong> built
+        from scratch.
+      </li>
+      <li>
+        Used <strong>SheetJS</strong> and <strong>Node</strong> to parse data
+        from <strong>Excel</strong> file.
+      </li>
+      <li>
+        Made logic for <strong>sorting</strong>, <strong>visualization</strong>,
+        &amp; <strong>interactivity</strong> using pure
+        <strong>JavaScript</strong>and used<strong>
+          HTML Template elements</strong
+        >.
+      </li>
+    </ul>
+    <p class="mt-1">
+      <u>
+        <strong>Technologies</strong>: HTML, EJS, CSS, SaSS, JavaScript
+        (Vanilla), and Node
+      </u>
+    </p>
+  </article>
+  <article class="project resume__article">
+    <div class="resume__articleHeading">
+      <h3>Interactive infographic component</h3>
+      <span>[<a href="" target="_blank">Live link</a>]</span>
+    </div>
+    <ul>
+      <li>
+        A standalone interactive and
+        <strong>responsive infographic component</strong> built for a UK
+        government&rsquo;s webpage.
+      </li>
+      <li>
+        Translated the design from <strong>Adobe Illustrator</strong> format to
+        absolutely positioned HTML image elements.
+      </li>
+      <li>
+        Used <strong>SaSS mixins</strong> to convert “pixel” units from
+        Illustrator to relative “percentages” and “positions” in CSS for
+        responsiveness.
+      </li>
+    </ul>
+    <p class="mt-1">
+      <u>
+        <strong>Technologies</strong>: HTML, CSS, SaSS, and JavaScript (Vanilla)
+      </u>
+    </p>
+  </article>
+  <article class="project resume__article">
+    <div class="resume__articleHeading">
+      <h3>Our Impact Plan &ndash; KPMG International</h3>
+      <span>[<a href="" target="_blank">Live link</a>]</span>
+    </div>
+    <ul>
+      <li>Website built for a flagship campaign hosted on AEM.</li>
+      <li>
+        Added <strong>scroll-based animations</strong>, built custom UI
+        components, and included
+        <strong>sticky positioning effect</strong> using JS.
+      </li>
+    </ul>
+    <p class="mt-1">
+      <u>
+        <strong>Technologies</strong>: HTML, EJS, CSS, SaSS, and JavaScript
+        (Vanilla)
+      </u>
+    </p>
   </article>
 </template>
