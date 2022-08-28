eae07ec377062993e4cd56434c06ee674f5b9870
Sukhvinder Singh <sukhvisukh@gmail.com>
2022-08-29 00:45:59 +0530

    Tested work section with dummy data

--- src/App/components/Work.vue
+++ src/App/components/Work.vue
@@ -2,49 +2,50 @@
   <div class="work__section">
     <div class="work__details">
       <div>
-        <h3 class="mb-0">Company name</h3>
-        <h4>Role in company</h4>
+        <h3 class="mb-0">KPMG</h3>
+        <h4>Front-end developer</h4>
       </div>
       <ul class="work__progression">
-        <li>Role (Start - End)</li>
-        <li>Role (Start - End)</li>
-        <li>Role (Start - End)</li>
+        <li><strong>Senior Analyst</strong> (Oct 2021 - Present)</li>
+        <li><strong>Business Associate</strong> (Oct 2020 - Sep 2021)</li>
+        <li><strong>Analyst</strong> (Jul 2018 - Sep 2020)</li>
       </ul>
     </div>
     <ul>
       <li>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatem,
-        quod dignissimos! Quas placeat enim commodi culpa, repellendus eaque!
-        Est, veritatis.
+        Development of <strong>Websites</strong>, <strong>Web Apps</strong>,
+        <strong>UI Components</strong> &amp;
+        <strong>Interactive Browser-based Proposals</strong> for KPMG UK
+        firm&apos;s clients &amp; KPMG&rsquo;s official website based on AEM.
       </li>
       <li>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatem,
-        quod dignissimos! Quas placeat enim commodi culpa, repellendus eaque!
-        Est, veritatis.
+        Developed &amp; maintaining internal
+        <strong>automation tools</strong> to enhance efficiency (<strong
+          >E-mail generator</strong
+        >
+        &amp; <strong>Custom Photoshop Plugin</strong>). [1]
       </li>
       <li>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatem,
-        quod dignissimos! Quas placeat enim commodi culpa, repellendus eaque!
-        Est, veritatis.
+        Following best practices &amp; standards &ndash; writing
+        <strong>maintainable, reusable, scalable, &amp; clean code</strong> in
+        JavaScript &amp; BEM (Block Element Modifier) for
+        <strong>reusable and scalable CSS</strong>.
       </li>
       <li>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatem,
-        quod dignissimos! Quas placeat enim commodi culpa, repellendus eaque!
-        Est, veritatis.
+        Enhanced internal processes based on <strong>Lean</strong> and
+        <strong>Six-sigma principles</strong> that led to an efficiency gain of
+        ~20-30%. [2]
       </li>
       <li>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatem,
-        quod dignissimos! Quas placeat enim commodi culpa, repellendus eaque!
-        Est, veritatis.
+        Built &amp; maintained
+        <strong>ready-to-use boilerplate templates</strong> for CMSs (Umbraco
+        and AEM) based on <strong>WebPack</strong> and <strong>PostCSS</strong>.
+        [2]
       </li>
       <li>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatem,
-        quod dignissimos! Quas placeat enim commodi culpa, repellendus eaque!
-        Est, veritatis.
+        Conducted <strong>internal training sessions</strong> &amp;
+        <strong>upskilled team members</strong>. [View certificate]
       </li>
     </ul>
   </div>
 </template>
-
-<script setup>
-</script>
