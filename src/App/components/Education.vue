d27873a1e82db81bc9d8e1c1a4ccdca0a78a955a
Sukhvinder Singh <sukhvisukh@gmail.com>
2022-08-29 02:34:10 +0530

    Added education section with dummy data

---
+++ src/App/components/Education.vue
@@ -0,0 +1,10 @@
+<template>
+  <article class="education resume__article">
+    <h3 class="mb-1">
+      Maharaja Surajmal Institute of Technology
+      <span :style="{ color: '#000' }">| [2014 &ndash; 2018]</span>
+    </h3>
+    <p class="mb-1" :style="{fontSize: `0.7rem`}">(Guru Gobind Singh Indraprashta University)</p>
+    <p><strong>B.Tech</strong> (Computer Science &amp; Engineering) | [<strong>GPA: 8.1</strong>]</p>
+  </article>
+</template>
