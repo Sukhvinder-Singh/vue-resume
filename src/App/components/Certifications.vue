fad2bead377f5f37581d79edd8ab513e08ece331
Sukhvinder Singh <sukhvisukh@gmail.com>
2022-08-29 21:57:30 +0530

    Added certifications section

---
+++ src/App/components/Certifications.vue
@@ -0,0 +1,28 @@
+<template>
+  <article class="certification resume__article">
+    <div class="resume__articleHeading">
+      <h3>Six Sigma Yellow Belt</h3>
+      <span>[<a href="" target="_blank">Link</a>]</span>
+    </div>
+    <ul>
+      <li>
+        Enhanced the internal processes using
+        <strong>Lean</strong> and <strong>Six Sigma principles</strong>.
+      </li>
+      <li>
+        Developed <strong>ready-to-use</strong> boilerplate templates that mimic
+        production environment in local setup, and
+        <strong>pre-coded styles in SaSS</strong> as per brand&apos;s identity.
+      </li>
+      <li>
+        Used <strong>Webpack</strong>, <strong>PostCSS</strong>, and
+        <strong>Babel</strong>, for <strong>VanillaJS</strong>,
+        <strong>Vue</strong>, and <strong>React</strong> implementations.
+      </li>
+      <li>
+        Achieved <strong>reduction in bugs</strong> and
+        <strong>cut development costs by ~25</strong>%.
+      </li>
+    </ul>
+  </article>
+</template>
