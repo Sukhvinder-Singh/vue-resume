1de71794d4a66d9d8e96e8b34395cdbc36e90b37
Sukhvinder Singh <sukhvisukh@gmail.com>
2022-08-30 02:04:27 +0530

    Interface for resume sections data added

---
+++ src/App/composables/resume-interface.js
@@ -0,0 +1,84 @@
+/**
+ * 
+ * @typedef {{section: String, heading: String, column: String, order: Number, display: Boolean, content: Array}} resumeSection 
+ */
+
+/**
+ * 
+ * @param {resumeSection} resumeSectionObject 
+ * @returns 
+ */
+export const resumeSectionInterface = resumeSectionObject => {
+
+    return {
+        /**
+         * Returns name of the section
+         * @returns {String}
+         */
+        getSection: () => resumeSectionObject.section,
+
+        /**
+         * Returns heading of the section
+         * @returns {String}
+         */
+        getHeading: () => resumeSectionObject.heading,
+
+        /**
+         * Returns column ("left" or "right")
+         * @returns {String}
+         */
+        getColumn: () => resumeSectionObject.column,
+
+        /**
+         * Returns order of appearance
+         * @returns {Number}
+         */
+        getOrder: () => resumeSectionObject.order,
+
+        /**
+         * Returns visibility of the section
+         * @returns {Boolean}
+         */
+        isVisible: () => resumeSectionObject.display,
+
+        /**
+         * Returns contents in the section
+         * @returns {Array<object>}
+         */
+        getContent: () => resumeSectionObject.content
+    }
+}
+
+/**
+ * Get all sections that are visible
+ * @param {Array<resumeSection>} data 
+ * @returns {Array} Visible sections array
+ */
+export const getVisibleSections = data => {
+    return data.filter(resumeSectionObject => {
+        return resumeSectionInterface(resumeSectionObject).isVisible() === true
+    });
+}
+
+/**
+ * Sort resume sections by thier order
+ * @param {Array<resumeSection>} data 
+ */
+export const sortSectionsByOrder = data => {
+    return data.sort((a, b) => {
+        const x = resumeSectionInterface(a).getOrder();
+        const y = resumeSectionInterface(b).getOrder();
+        return x - y;
+    });
+}
+
+/**
+ * Get left or right column sections
+ * @param {Array<resumeSection>} resumeData
+ * @param {String} column 
+ */
+export const getColumnSections = (resumeData, column) => {
+    return resumeData.filter(resumeSectionObject => {
+        return resumeSectionInterface(resumeSectionObject).getColumn() === column
+    });
+}
