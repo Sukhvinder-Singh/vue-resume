766e7fb8f53424467c493670f89c3a258a6bba5b
Sukhvinder Singh <sukhvisukh@gmail.com>
2022-08-29 21:57:42 +0530

    Added achievements section

---
+++ src/App/components/Achievement.vue
@@ -0,0 +1,25 @@
+<template>
+  <article class="certification resume__article">
+    <div class="resume__articleHeading">
+      <h3>Innovation award</h3>
+      <span>[<a href="" target="_blank">Link</a>]</span>
+    </div>
+    <ul>
+      <li>
+        Made an <strong>automatic e-mail template generator for</strong>MS Word
+        format in <strong>Vue.</strong>
+      </li>
+
+      <li>
+        Developed a <strong>Photoshop CEP plugin</strong> using
+        <strong>JavaScript</strong> to automate the generation of social media
+        banners as per KPMG&rsquo;s identity.
+      </li>
+
+      <li>
+        Reduced repetitive tasks using automation that led to an
+        <strong>efficiency gain of ~50-60</strong>%.
+      </li>
+    </ul>
+  </article>
+</template>
