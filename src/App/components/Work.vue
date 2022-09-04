6e57d9c28a210b863bfc13cb894e5dfaa52c219e
Sukhvinder Singh <sukhvisukh@gmail.com>
2022-09-05 00:41:09 +0530

    Added dynamic data for work experience

--- src/App/components/Work.vue
+++ src/App/components/Work.vue
@@ -1,51 +1,36 @@
 <template>
-  <article class="work resume__article">
+  <article v-for="workData in data" class="work resume__article">
     <div class="work__details">
       <div>
-        <h3 class="mb-0">KPMG</h3>
-        <h4>Front-end developer</h4>
+        <h3 class="mb-0">
+          {{ workExperience(workData).getOrganization() }}
+        </h3>
+        <h4>{{ workExperience(workData).getRole() }}</h4>
       </div>
       <ul class="work__progression">
-        <li><strong>Senior Analyst</strong> (Oct 2021 - Present)</li>
-        <li><strong>Business Associate</strong> (Oct 2020 - Sep 2021)</li>
-        <li><strong>Analyst</strong> (Jul 2018 - Sep 2020)</li>
+        <li v-for="progression in workExperience(workData).getProgression()">
+          <strong>{{ progressionData(progression).getRole() }}</strong>
+          ({{ progressionData(progression).getStart() }} -
+          {{ progressionData(progression).getEnd() }})
+        </li>
       </ul>
     </div>
     <ul>
-      <li>
-        Development of <strong>Websites</strong>, <strong>Web Apps</strong>,
-        <strong>UI Components</strong> &amp;
-        <strong>Interactive Browser-based Proposals</strong> for KPMG UK
-        firm&apos;s clients &amp; KPMG&rsquo;s official website based on AEM.
-      </li>
-      <li>
-        Developed &amp; maintaining internal
-        <strong>automation tools</strong> to enhance efficiency (<strong
-          >E-mail generator</strong
-        >
-        &amp; <strong>Custom Photoshop Plugin</strong>). [1]
-      </li>
-      <li>
-        Following best practices &amp; standards &ndash; writing
-        <strong>maintainable, reusable, scalable, &amp; clean code</strong> in
-        JavaScript &amp; BEM (Block Element Modifier) for
-        <strong>reusable and scalable CSS</strong>.
-      </li>
-      <li>
-        Enhanced internal processes based on <strong>Lean</strong> and
-        <strong>Six-sigma principles</strong> that led to an efficiency gain of
-        ~20-30%. [2]
-      </li>
-      <li>
-        Built &amp; maintained
-        <strong>ready-to-use boilerplate templates</strong> for CMSs (Umbraco
-        and AEM) based on <strong>WebPack</strong> and <strong>PostCSS</strong>.
-        [2]
-      </li>
-      <li>
-        Conducted <strong>internal training sessions</strong> &amp;
-        <strong>upskilled team members</strong>. [View certificate]
-      </li>
+      <li
+        v-for="detail in workExperience(workData).getDetails()"
+        v-html="detail"
+      ></li>
     </ul>
   </article>
 </template>
+
+<script setup>
+import {
+  workExperience,
+  progressionData,
+} from "../composables/work-experience";
+
+const props = defineProps({
+  data: Object,
+});
+</script>
--- src/App/composables/data.js
+++ src/App/composables/data.js
@@ -10,28 +10,31 @@ export const RESUME_DATA = [
                 order: 1,
                 display: true,
                 organization: "KPMG",
-                currentRole: "Front-end developer",
+                role: "Front-end developer",
                 progression: [
                     {
-                        role: "A",
-                        startDate: "",
-                        endDate: "",
+                        role: "Senior Analyst",
+                        start: "Oct 2021",
+                        end: "Present",
                     },
                     {
-                        role: "BA",
-                        startDate: "",
-                        endDate: "",
+                        role: "Business Associate",
+                        start: "Oct 2020",
+                        end: "Sep 2021",
                     },
                     {
-                        role: "SA",
-                        startDate: "",
-                        endDate: "",
+                        role: "Analyst",
+                        start: "Jul 2018",
+                        end: "Sep 2020",
                     }
                 ],
                 details: [
-                    `A`,
-                    `AA`,
-                    `AAA`
+                    `Development of <strong>Websites</strong>, <strong>Web Apps</strong>, <strong>UI Components</strong> &amp; <strong>Interactive Browser-based Proposals</strong> for KPMG UK firm&apos;s clients &amp; KPMG&rsquo;s official website based on AEM.`,
+                    `Developed &amp; maintaining internal <strong>automation tools</strong> to enhance efficiency (<strong>E-mail generator</strong> &amp; <strong>Custom Photoshop Plugin</strong>). [1]`,
+                    `Following best practices &amp; standards &ndash; writing <strong>maintainable, reusable, scalable, &amp; clean code</strong> in JavaScript &amp; BEM (Block Element Modifier) for <strong>reusable and scalable CSS</strong>.`,
+                    `Enhanced internal processes based on <strong>Lean</strong> and <strong>Six-sigma principles</strong> that led to an efficiency gain of ~20-30%. [2]`,
+                    `Built &amp; maintained <strong>ready-to-use boilerplate templates</strong> for CMSs (Umbraco and AEM) based on <strong>WebPack</strong> and <strong>PostCSS</strong>. [2]`,
+                    `Conducted <strong>internal training sessions</strong> &amp; <strong>upskilled team members</strong>. [View certificate]`
                 ]
             }
         ],
