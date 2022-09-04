8c7a29b5497e0d0816c31ad05a5e94ca30bc67b0
Sukhvinder Singh <sukhvisukh@gmail.com>
2022-09-04 22:24:41 +0530

    added dummy data for testing

---
+++ src/App/composables/data.js
@@ -0,0 +1,46 @@
+export const RESUME_DATA = [
+    {
+        section: "work-experience",
+        heading: "Work experience",
+        column: "left",
+        order: 1,
+        display: true,
+        content: [
+            {
+                order: 1,
+                display: true,
+                organization: "KPMG",
+                currentRole: "Front-end developer",
+                progression: [
+                    {
+                        role: "A",
+                        startDate: "",
+                        endDate: "",
+                    },
+                    {
+                        role: "BA",
+                        startDate: "",
+                        endDate: "",
+                    },
+                    {
+                        role: "SA",
+                        startDate: "",
+                        endDate: "",
+                    }
+                ],
+                details: [
+                    `A`,
+                    `AA`,
+                    `AAA`
+                ]
+            }
+        ],
+    },
+    {
+        section: "projects",
+        heading: "Projects",
+        column: "left",
+        display: true,
+        order: 2
+    }
+]
